7f5b352d20de85843292c7f2c9835e557433f70b
Covaar <co.va.ard@gmail.com>
2024-12-10 06:20:53 -0500

    Second commit

--- src/avSlice.js
+++ src/avSlice.js
@@ -1,20 +1,57 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 export const avSlice = createSlice({
-  name: "av",
-  initialState: [
-    
-  ],
+    name: "av",
+    initialState: [
+        {
+            img: "",
+            name: "Proyectores",
+            cost: 200,
+            quantity: 0,
+        },
+        {
+            img: "",
+            name: "Altavoz",
+            cost: 35,
+            quantity: 0,
+        },
+        {
+            img: "",
+            name: "Micrófonos",
+            cost: 45,
+            quantity: 0,
+        },
+        {
+            img: "",
+            name: "Pizarras",
+            cost: 80,
+            quantity: 0,
+        },
+
+        {
+            img: "",
+            name: "Señalización",
+            cost: 80,
+            quantity: 0,
+        },
+
+    ],
 
 
-  reducers: {
-    incrementAvQuantity: (state, action) => {
-      
+    reducers: {
+        incrementAvQuantity: (state, action) => {
+            const item = state[action.payload];
+            if (item) {
+                item.quantity++;
+            }
+        },
+        decrementAvQuantity: (state, action) => {
+            const item = state[action.payload];
+            if (item && item.quantity > 0) {
+                item.quantity--;
+            }
+        },
     },
-    decrementAvQuantity: (state, action) => {
-     
-    },
-  },
 });
 
 export const { incrementAvQuantity, decrementAvQuantity } = avSlice.actions;
